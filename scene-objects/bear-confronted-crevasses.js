9b006b6fd2642c1dd73272b2c65fd73437637748
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2022-11-18 15:02:06 +0100

    Added injured and left behind teammate speech.

--- scene-objects/bear-confronted-crevasses.js
+++ scene-objects/bear-confronted-crevasses.js
@@ -1,9 +1,9 @@
 const bearConfrontedCrevasses = {
     travel: false,
     text: 'A few seconds of adrenaline feel like a lifetime. You and your team manage to fend the creature off but not unscathed. One of you is injured. If you <span>bring them</span> with you travelling will take twice as long. You can keep up the pace if you <span>leave them behind</span> to their death. The strange compulsion drives you forwards.',
-    nielsenText:'',
-    kleistText:'',
-    overgaardText:'',
+    nielsenText:'We don’t have forever, we should prioritise our research. I want to check the crevasses for any surviving wildlife.',
+    kleistText:'If we can get a better look at that electrical storm, the reading may well give me some insight into what happened here.',
+    overgaardText:'We’re on too tight a schedule. The company wants me to report the cause of the incident. The data suggests it was straight ahead, that’s where we should go.',
     bringThem() {
         loadScene(findPilot2);
     },
--- scene-objects/global.js
+++ scene-objects/global.js
@@ -1,13 +1,34 @@
 /** Contains methods common to all scenes */
 const global = {
+    injuredGroan: '*groans*',
     nielsenThoughts() {
-        sceneText.innerHTML = scene.nielsenText;
+        console.log(injured);
+        console.log(leftBehind);
+        if (injured === 1 && leftBehind === 0) {
+            sceneText.innerHTML = this.injuredGroan;
+        } else if (leftBehind === 1) {
+            sceneText.innerHTML = 'nielsen has been abandoned to the crater';
+        } else {
+            sceneText.innerHTML = scene.nielsenText;
+        }
     },
     kleistThoughts() {
-        sceneText.innerHTML = scene.kleistText;
+        if (injured >= 1 && leftBehind === 1) {
+            sceneText.innerHTML = this.injuredGroan;
+        } else if (leftBehind === 2) {
+            sceneText.innerHTML = 'kleist has been abandoned to the crater';
+        } else {
+            sceneText.innerHTML = scene.kleistText;
+        }
     },
     overgaardThoughts() {
-        sceneText.innerHTML = scene.kleistText;
+        if (injured >= 1 && leftBehind === 2) {
+            sceneText.innerHTML = this.injuredGroan;
+        } else if (leftBehind === 3) {
+            sceneText.innerHTML = 'overgaard has been abandoned to the crater';
+        } else {
+            sceneText.innerHTML = scene.overgaardText;
+        }
     },
     back() {
         sceneText.innerHTML = scene.text;
